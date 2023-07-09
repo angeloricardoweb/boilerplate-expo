b5413cd1538634ea7fb48d328ac3281399babc06
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-08 23:33:32 -0300

    progress: working in expo boilerplate

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-import "./src/lib/dayjs"
-
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
-import Loading from './src/components/Loading';
 import { Home } from './src/screens/Home';
+import Loading from "./src/components/Loading";
 // import OneSignal from 'react-native-onesignal';
 // OneSignal.setAppId("api-key");
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,8 +3,8 @@ import { View, Text, ScrollView } from 'react-native'
 
 export const Home: FC = () => {
   return (
-    <View className='flex justify-center items-center'>
-      <Text>Expo Boilerplate</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-black'>Expo Boilerplate</Text>
     </View>
   )
 }
