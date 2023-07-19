f11c8cae4cdbc67ec845e9bbe2a49318aff0c4ca
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-19 00:46:16 -0300

    feat: add module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel']
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@assets': './src/assets',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@utils': './src/utils',
+            '@hooks': './src/hooks',
+            '@storage': './src/storage',
+          },
+        },
+      ],
+    ],
   };
 };
