d09809e2a23cd1dda968043eda3f44f71fe1ff89
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-29 17:12:18 -0300

    chore: configure react-query provider

--- App.tsx
+++ App.tsx
@@ -1,30 +1,40 @@
 import { StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
-import { Home } from './src/screens/Home';
-import Loading from "./src/components/Loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import Loading from './src/components/Loading';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Routes } from 'src/routes';
 // import OneSignal from 'react-native-onesignal';
 // OneSignal.setAppId("api-key");
 // OneSignal.promptForPushNotificationsWithUserResponse()
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold
-  })
-
+    Inter_800ExtraBold,
+  });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    )
-  };
+    return <Loading />;
+  }
 
   return (
-    <>
-      <Home />
-      <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor="transparent"
+        translucent
+      />
+    </QueryClientProvider>
   );
 }
