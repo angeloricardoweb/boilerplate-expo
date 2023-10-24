acb1a6c4cabbf4df95d46928d5f583b484cded8f
Angelo Ricardo <angeloengcomp@gmail.com>
2023-10-24 10:05:36 -0300

    ui: add StarRating component

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,31 @@
+import { FontAwesome } from '@expo/vector-icons';
+import { View } from 'react-native';
+
+export function StarRating({ starsValue }: { starsValue: number }) {
+  const starIcons = [];
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= starsValue) {
+      // Renderiza estrela completa
+      starIcons.push(
+        <FontAwesome key={i} name="star" size={12} color="yellow" />
+      );
+    } else if (i - 0.5 === starsValue) {
+      // Renderiza meia estrela
+      starIcons.push(
+        <FontAwesome key={i} name="star-half" size={12} color="yellow" />
+      );
+    } else {
+      // Renderiza estrela vazia
+      starIcons.push(
+        <FontAwesome key={i} name="star-o" size={12} color="yellow" />
+      );
+    }
+  }
+
+  return (
+    <View className="flex-row gap-1 absolute top-8 right-5">{starIcons}</View>
+  );
+}
+
+//   <StarRating starsValue={4.5} />
