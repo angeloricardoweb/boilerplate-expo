11c8b1e6e93e6d6520060a6ddb5aabbaf5d9d1cd
Angelo Ricardo <angeloengcomp@gmail.com>
2024-09-04 18:55:28 -0300

    refactor: add authorization header to axios requests

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,26 @@
 import { API_CONTRACT } from '@constants/index';
 import axios from 'axios';
+import { getToken } from '@storage/token';
 
 export const api = axios.create({
   baseURL: API_CONTRACT,
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await getToken();
+
+      if (token) {
+        config.headers.Authorization = `${token}`;
+      }
+
+      return config;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
